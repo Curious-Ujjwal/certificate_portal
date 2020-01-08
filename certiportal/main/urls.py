13a83bff3667f345cda0bd393dd6ec7b5fe5b52c
Pranav Gupta <pranavgupta000@gmail.com>
2020-01-08 16:50:07 +0530

    url based pdf rendering done

--- certiportal/main/urls.py
+++ certiportal/main/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index , name="index"),
     path('accounts/logout/', views.logoutView , name="logoutView"),
+    re_path(r'^certificate/(?P<cert_id>ALC-[A-Z]{3}-[0-9]+-20[0-9]{2}-[0-9]+)/$', views.certificate , name="certificate"),
 ]
 
--- certiportal/main/views.py
+++ certiportal/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
+from .models import *
+from .render import Render
+
 
 # Create your views here.
 def index(request):
@@ -8,4 +11,22 @@ def index(request):
 def logoutView(request):
     logout(request)
     return render(request , 'registration/logout.html')
+
+
+
+def certificate(request, cert_id):
+    
+    try:
+        candid = candidate.objects.get(certificate_url=cert_id)
+    except candidate.DoesNotExist:
+        candid = None
+
+    if not candid or not candid.is_valid:
+    	# DO SOMETHING 
+    	return render(request, 'registration/index.html')
+
+    context = {
+        candid = candid,
+    }
+    return Render.render('certificate/certificateParticipant.html', context)
 
