ed2b2171cab56bdc5c76703a914c50fc3b16628b
Pranav <pranavgupta000@gmail.com>
2020-01-12 01:19:16 +0530

    added bulk data add link

--- certiportal/main/urls.py
+++ certiportal/main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index , name="index"),
     path('candidform', views.candidForm , name="candidForm"),
     path('candidlist', views.candidList , name="candidList"),
+    path('candidbulk', views.candidBulk , name="candidBulk"),
     path('accounts/logout/', views.logoutView , name="logoutView"),
     path('mail_sent', views.send_email , name="send_email"),
     re_path(r'^certificate/(?P<cert_id>ALC-[A-Z]{3}-[0-9]+-20[0-9]{2}-[0-9]+)/$', views.certificate , name="certificate"),
